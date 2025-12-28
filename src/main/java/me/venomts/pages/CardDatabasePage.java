96065a8ad552e6bd54f29ed6a69a09ed5f6bb1d3
AStalinist <pismoahmed@gmail.com>
2025-12-28 18:30:05 +0100

    Fix: Selectors for filters

--- src/main/java/me/venomts/pages/CardDatabasePage.java
+++ src/main/java/me/venomts/pages/CardDatabasePage.java
@@ -12,7 +12,7 @@ public class CardDatabasePage
 
     private Locator _filterAttribute;
     private Locator _filterRace;
-    private Locator _advancedFiltersButton;
+    private Locator _AdvancedFiltersButton;
 
 
     public CardDatabasePage(Page page)
@@ -20,13 +20,15 @@ public class CardDatabasePage
         this.page = page;
         _filterAttribute = page.locator("#filter-attribute");
         _filterRace = page.locator("#filter-race");
-        _advancedFiltersButton = page.locator(".btn.btn-primary.filterlimit");
+        _AdvancedFiltersButton = page.locator(".btn.btn-primary.filterlimit ");
     }
 
     //Used only inside this class:
-    private void ToggleAdvancedFiltersButton()
+    public void ToggleAdvancedFiltersButton()
     {
-        _advancedFiltersButton.click();
+        String filterButtonExpanded = _AdvancedFiltersButton.getAttribute("aria-expanded");
+        if ("false".equals(filterButtonExpanded))
+            _AdvancedFiltersButton.click();
     }
     //In actual tests:
     public void SelectAttribute(String attribute)
@@ -38,13 +40,11 @@ public class CardDatabasePage
     public void SelectRace(String race)
     {
         ToggleAdvancedFiltersButton();
-        _filterRace.click();
-        _filterRace.getByText(race).click();
+        _filterRace.selectOption(race);
     }
 
     public void AssertFiltersApplied()
     {
-        String currentURL = page.url();
         assertThat(page).hasURL("https://ygoprodeck.com/card-database/?attribute=DIVINE&race=Aqua&num=24&offset=0");
     }
 
